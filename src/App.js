3b55b40091492071b15ecba58d71c25051f45f62
Daniel Lin <david82213@hotmail.com>
2017-03-05 16:56:26 -0800

    added some custom validation methods

--- src/App.js
+++ src/App.js
@@ -17,16 +17,27 @@ class App extends React.Component {
     return (
       <div>
         <h1>Welcome to PermaBand! We have {this.state.count} guild mates</h1>
-        <input type="number" onChange={this.update.bind(this)} />
+        {/* <input type="number" onChange={this.update.bind(this)} /> */}
+        <Counter update={this.update.bind(this)} />
       </div>
     )
   }
 }
 
 // const App = () => <h1>Welcome to PermaBand</h1>
+const Counter = (props) =>
+  <input type="number" onChange={props.update} />
 
 App.propTypes = {
-  count: React.PropTypes.number.isRequired
+  // count: React.PropTypes.number.isRequired
+  count(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName] < 0){
+      return new Error(`${propName} cannot be less than 0`)
+    }
+  }
 }
 
 // default value of count if no count is passed in
